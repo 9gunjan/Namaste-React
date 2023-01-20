f9b0267f3d63196dd5898765187670673eea05cd
Gunjan Thakur <Gunjant825@gmail.com>
2023-01-20 13:41:08 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+const Heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Heading 1"
+);
+
+const Heading2 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Heading 2"
+);
+
+const container = React.createElement("div", {id : "container"}, [Heading, Heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//passing a react element inside the root
+root.render(container);
